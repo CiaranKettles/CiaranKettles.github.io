b1d82451c1549080d420bed2205e958358e7a292
CiaranKettles <144144766+CiaranKettles@users.noreply.github.com>
2025-11-23 18:21:41 -0500

    Update main.js
    added image array and base url variable

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -3,3 +3,8 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const images = [{filename: "pic1.jpg", alt: "closeup shot of a person's face"}, {filename: "pic2.jpg", alt: "rippling, beige rock"}, {filename: "pic3.jpg", alt: "a closup of white and purple flowers"}, {filename: "pic4.jpg", alt: "an egyptian mural"}, {filename: "pic5.jpg", alt: "a butterfly on a leaf"}];
+
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
